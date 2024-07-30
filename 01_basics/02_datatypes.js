a91d8a56cf21a094c3c516572ba926ade5a19dce
shweta1693 <131758668+shweta1693@users.noreply.github.com>
2024-07-30 08:52:00 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,26 @@
 "use strict"//treat all JS code as newer version
-alert(3+3)
+//alert(3+3) // we are using nodejs, not browser
+console.log(3+3); console.log("Shweta")
+
+console.log(3
+    +
+    3)//code readiblity should be high
+console.log("Shweta")
+
+let name="Shweta"
+let age =24
+let isLoggedIn="false"
+//number=>2 to power 53 //range
+//bigint
+//string=>
+//boolean=>true/false
+//null=>standalone value
+//undefined //value not assigned yet
+//symbol=>unique
+
+
+//object
+
+console.log(typeof "Shweta")
+console.log(typeof age)
+console.log(typeof null)
