5fb58925bec61eeedd2ccc9c638d41b00cc81fdb
shweta1693 <131758668+shweta1693@users.noreply.github.com>
2024-07-30 07:48:57 +0000

    variable and constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="shwetasharma16@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=2  //not allowed
+accountEmail="hchc@gmail.com";
+accountPassword="212121"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,2 @@
+"use strict"//treat all JS code as newer version
+alert(3+3)
