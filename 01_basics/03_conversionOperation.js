1c865793fa017ecd388062b245ec585f4039f572
shweta1693 <131758668+shweta1693@users.noreply.github.com>
2024-07-31 05:40:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score=33;
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+//""=>0:"Shweta"=>1
+let isLoggedIn ="Shweta"
+let boolenLoggedIn= Boolean(isLoggedIn)
+console.log(boolenLoggedIn)
